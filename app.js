273928a340bf2b54abd2b83f4cfeb3accab20020
13circle <13circle97@gmail.com>
2020-10-08 00:46:32 +0900

    Add fetchdataRouter to app.use

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/fetchdata", fetchdataRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

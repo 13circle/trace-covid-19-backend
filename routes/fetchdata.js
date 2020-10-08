a86dbef7c65de8a84c2a3189a2226fb421e76ec5
13circle <13circle97@gmail.com>
2020-10-09 03:36:11 +0900

    Add form-data module & fetchdata.js in progress

--- routes/fetchdata.js
+++ routes/fetchdata.js
@@ -1,10 +1,27 @@
 var express = require("express");
+var FormData = require("form-data");
+var axios = require("axios");
 var router = express.Router();
 
-var axios = require("axios");
+var SEOUL_COVID_JSON_URL = "http://odc.data.seoul.go.kr/bigfile/iot/sheet/json/download.do";
 
-router.get("/", function(req, res, next) {
-	res.send("This is fetchdata API");
+async function getCovidData() {
+  var form = new FormData();
+  form.append("srvType", "S");
+  form.append("infId", "OA-20279");
+  form.append("serviceKind", 1);
+  return await axios.post(
+    SEOUL_COVID_JSON_URL,
+    form,
+    {
+      headers: form.getHeaders(),
+    }
+  );
+}
+
+router.get("/", async function(req, res, next) {
+  var resData = await getCovidData();
+	res.send(JSON.stringify(resData.data, null, 2));
 });
 
 module.exports = router;

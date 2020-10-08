f7dc2234ac5b6d4967531140cf65545bc475b1e6
13circle <13circle97@gmail.com>
2020-10-08 23:50:43 +0900

    Add Axios module for fetching

--- routes/fetchdata.js
+++ routes/fetchdata.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = express.Router();
 
+var axios = require("axios");
+
 router.get("/", function(req, res, next) {
 	res.send("This is fetchdata API");
 });

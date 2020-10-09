00b5a2de1ac1b561f744e4f6d8748584fd16e5f2
13circle <13circle97@gmail.com>
2020-10-10 00:15:25 +0900

    Separate URLs & Minimize json response

--- routes/fetchdata.js
+++ routes/fetchdata.js
@@ -19,9 +19,27 @@ async function getCovidData() {
   );
 }
 
-router.get("/covid-19-json", async function(req, res, next) {
+router.get("/covid19-description", async function(req, res, next) {
   var resData = await getCovidData();
-	res.send(JSON.stringify(resData.data));
+  res.send(JSON.stringify(resData.data.DESCRIPTION));
+});
+
+router.get("/covid19-data", async function(req, res, next) {
+  var resData = await getCovidData();
+	res.send(JSON.stringify(resData.data.DATA.map((d) => {
+    var corona19_date_str = (new String(d.corona19_date));
+    var corona19_date_m = corona19_date_str.split(".").shift();
+    var corona19_date_d = corona19_date_str.split(".").pop();
+    var corona19_date_timestamp = (new Date(
+      2020, corona19_date_m - 1, corona19_date_d
+    )).getTime();
+    return {
+      corona19_id: d.corona19_id,
+      corona19_leave_status: d.corona19_leave_status,
+      corona19_area: d.corona19_area,
+      corona19_date: corona19_date_timestamp,
+    };
+  })));
 });
 
 module.exports = router;
